0f1fa6a0772b87ff9d9d47135086b4ec61fa1c0c
Ye Htet <yhtet1934@gmail.com>
2026-02-06 20:58:47 +0800

    chore(ci): add require depencies for ci

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    '9b38cf48e83f5d8f60375221cd213eee' => $vendorDir . '/phpstan/phpstan/bootstrap.php',
     'fc8bb7cba02a11ab8591dc6d6548e20a' => $baseDir . '/app/helpers.php',
 );
